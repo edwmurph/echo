4c5956f640507402ef6ed0304e990fbd3100462b
edwmurph <20996513+edwmurph@users.noreply.github.com>
2023-05-31 18:40:39 -0400

    add aws cli

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "<center><h2>$(curl ${ECS_CONTAINER_METADATA_URI_V4})</h2></center>" > public/index.html
+echo "<center>" > public/index.html
+echo "<h2>$(aws --version)</h2>" >> public/index.html
+echo "<br />" > public/index.html
+echo "<h2>$(aws sts get-caller-identity)</h2>" >> public/index.html
+echo "<br />" > public/index.html
+echo "<h2>$(curl ${ECS_CONTAINER_METADATA_URI_V4})</h2>" >> public/index.html
+echo "</center>" >> public/index.html
 
 node index.mjs
