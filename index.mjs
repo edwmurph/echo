ff06c694d746127435d6a03d98852354f4d2a593
edwmurph <20996513+edwmurph@users.noreply.github.com>
2023-05-31 20:44:56 -0400

    add ssm route

--- index.mjs
+++ index.mjs
@@ -1,4 +1,7 @@
 import express from 'express';
+import { SSMClient, GetParametersCommand } from '@aws-sdk/client-ssm';
+
+const ssm_client = new SSMClient();
 
 const PORT = 80;
 
@@ -7,5 +10,16 @@ const app = express();
 app.use( express.static('public') );
 
 app.get( '/hello', ( _, res ) => res.send('Hello World!') );
+
+app.get( '/ssm', async( _, res ) => {
+  const command = new GetParametersCommand({
+    Names: [ 'test' ],
+    WithDecryption: true || false
+  });
+
+  const response = await ssm_client.send( command );
+
+  res.send( response );
+});
 
 app.listen( PORT, () => console.log( `listening at http://localhost:${ PORT }` ) );
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "<center>" > public/index.html
+echo "<center>" >> public/index.html
 echo "<h2>$(aws --version)</h2>" >> public/index.html
-echo "<br />" > public/index.html
+echo "<br />" >> public/index.html
 echo "<h2>$(aws sts get-caller-identity)</h2>" >> public/index.html
-echo "<br />" > public/index.html
+echo "<br />" >> public/index.html
 echo "<h2>$(curl ${ECS_CONTAINER_METADATA_URI_V4})</h2>" >> public/index.html
 echo "</center>" >> public/index.html
 
